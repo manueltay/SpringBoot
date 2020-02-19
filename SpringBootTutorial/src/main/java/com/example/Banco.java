c5867b4076f8dcaec233a2aa513ff7768912a655
Manuel Tay <mtay@sib.gob.gt>
2020-02-19 10:19:56 -0600

    feat: se agrega la clase banco para el control de bancos

---
+++ SpringBootTutorial/src/main/java/com/example/Banco.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import java.math.BigDecimal;
+
+public class Banco {
+	String nombre;
+	BigDecimal cantidad;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public BigDecimal getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(BigDecimal cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+	
+
+}
